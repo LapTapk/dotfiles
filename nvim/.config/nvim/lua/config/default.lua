e76047492978b485179b4dbb6577fe28facce1bc
apulaziuh <shit@hole.exe>
2025-01-02 16:26:23 +0300

    Added nvim

---
+++ nvim/.config/nvim/lua/config/default.lua
@@ -0,0 +1,8 @@
+vim.opt.nu = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.g.mapleader = '<Space>'
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
